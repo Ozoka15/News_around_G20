622459b078dc1a2fd78ea199dd2a62e4a9852516
Ozoka15 <110171037+Ozoka15@users.noreply.github.com>
2022-09-02 14:13:29 +0300

    Telegram_bot_news_around_G20
    This Telegram_news_bot.py for news about the G20 countries. After the "/start" command, you can start a dialogue with the Telegram bot. You need to respond to the Telegram bot's greeting and then click on the buttons with a choice of country, topic and number of news items. You can select the following G20 countries: Argentina, Australia, Brazil, Canada, China, European Union, France, Germany, India, Indonesia, Italy, Japan, Mexico, Russia, Saudi Arabia, South Korea, South Africa, Turkey, UK, USA; and find out news about the selected country. You can also choose from the following categories to get the news you want: economy, politics, society, security, environment, science, sport, culture and arts, celebrities, foreign policy. Then you can select the number of news items you want to receive. At the end you must write "news" to get the Telegram bot to send you news with your requests in mind. However, if you write another word in place of "news", you will get a certain picture that you definitely want to see. If you want to create your own Telegram news bot, you will need to get the api_key for Telebot and NewsApiClient. If you want to talk to my Telegram bot, use @News_around_the_world_bot.

--- Telegram_news_bot.py
+++ Telegram_news_bot.py
@@ -1,18 +1,20 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import telebot
-from bs4 import BeautifulSoup as bs
 import requests
 import json
-from newsapi import NewsApiClient
+import random as rd
+import fake_useragent
+from bs4 import BeautifulSoup as bs
+from newsapi.newsapi_client import NewsApiClient
+from fake_useragent import UserAgent
+from fake_useragent import FakeUserAgentError
 
 
 bot = telebot.TeleBot('5568824717:AAEl0bNR7dF0i64B74OHxn7icyOX8GLCk7Y')
 newsapi = NewsApiClient(api_key = '52a2b572ebf549149b492212fa1d789d')
+
+ua = fake_useragent.UserAgent()
+headers = {'User-Agent': ua.random}
 
 
 from telebot import types
@@ -21,51 +23,61 @@ from datetime import datetime
 
 @bot.message_handler(commands=['start'])
 def get_started(message):
-    first_reaction = bot.send_message(message.chat.id, 'Привет :)')
+    first_reaction = bot.send_message(message.chat.id, 'Hello! You can now catch up with news about one of the G20 countries. Write me something :)')
     bot.register_next_step_handler(first_reaction, set_country)
-    
+
 def set_country(message):
-    countries = ['australia', 'argentina', 'brazil', 'canada', 'china', 'eu', 'france',
-                  'germany', 'india', 'indonesia', 'italy', 'japan', 'mexico', 'russia',
-                  'saudi arabia', 'south korea', 'south africa', 'turkey', 'uk', 'usa']
+    countries = ['Australia', 'Argentina', 'Brazil', 'Canada', 'China', 'EU', 'France',
+                  'Germany', 'India', 'Indonesia', 'Italy', 'Japan', 'Mexico', 'Russia',
+                  'Saudi Arabia', 'South Korea', 'South Africa', 'Turkey', 'UK', 'USA']
     keyboard = types.InlineKeyboardMarkup()
-    message_info = bot.send_message(message.from_user.id, text = 'Выбери страну:')
+    message_info = bot.send_message(message.from_user.id, text = 'Choose the country:')
     message_id = message_info.id
-    for i in countries:
-        keyboard.add(types.InlineKeyboardButton(text = i, callback_data = 'country_'+i+'_'+str(message_id)))
-    bot.edit_message_text(chat_id = message.from_user.id, message_id = message_id, text = 'Выбери страну:', reply_markup = keyboard)
+    for i in range(0, len(countries), 2):
+        buttons = []
+        buttons.append(types.InlineKeyboardButton(text = countries[i], callback_data = 'country_'+countries[i]+'_'+str(message_id)))
+        buttons.append(types.InlineKeyboardButton(text = countries[i+1], callback_data = 'country_'+countries[i+1]+'_'+str(message_id)))
+        keyboard.add(*buttons)
+    bot.edit_message_text(chat_id = message.from_user.id, message_id = message_id, text = 'Choose the country:', reply_markup = keyboard)
 
 @bot.callback_query_handler(func = lambda call: True)
 def callback_worker(call):
     categories = ['economy', 'policy', 'society', 'security', 'ecology', 'science', 'sport', 'culture and art', 'celebrities', 'foreign policy']
     user_id = call.from_user.id
     if call.data.split('_')[0] == 'country':
-        bot.send_message(call.from_user.id, 'Молодец!')
         users = json_communicate('r', 'my_dick.json')
         users[str(call.message.chat.id)] = {'country': call.data.split('_')[1]}
         users[str(call.from_user.id)]['id_of_message'] = call.data.split('_')[2]
         json_communicate('w', 'my_dick.json', users)
         keyboard = types.InlineKeyboardMarkup()
-        for o in categories:
-            keyboard.add(types.InlineKeyboardButton(text = o, callback_data = 'category_'+o))
+        for o in range (0, len(categories), 2):
+            knopki = []
+            knopki.append(types.InlineKeyboardButton(text = categories[o], callback_data = 'category_'+categories[o]+'_'+str(user_id)))
+            knopki.append(types.InlineKeyboardButton(text = categories[o+1], callback_data = 'category_'+categories[o+1]+'_'+str(user_id)))
+            keyboard.add(*knopki)
         with open('my_dick.json') as trash:
             goal = json.load(trash)
-        bot.edit_message_text(chat_id = call.from_user.id, message_id = goal[str(user_id)]["id_of_message"], text = 'Выбери категорию:', reply_markup = keyboard)
+        bot.edit_message_text(chat_id = call.from_user.id, message_id = goal[str(user_id)]["id_of_message"], text = 'Choose the category:', reply_markup = keyboard)
     if call.data.split('_')[0] == 'category':
         users = json_communicate('r', 'my_dick.json')
         users[str(call.from_user.id)]['category'] = call.data.split('_')[1]
-        json_communicate('w', 'my_dick.json', users)  
+        json_communicate('w', 'my_dick.json', users)
         keyboard = types.InlineKeyboardMarkup()
-        for w in range(1,11):
-            keyboard.add(types.InlineKeyboardButton(text = w, callback_data = 'amount_'+str(w)))
+        for w in range(1, 11, 2):
+            stuff = []
+            stuff.append(types.InlineKeyboardButton(text = w, callback_data = 'amount_'+str(w)+'_'+str(user_id)))
+            stuff.append(types.InlineKeyboardButton(text = w+1, callback_data = 'amount_'+str(w+1)+'_'+str(user_id)))
+            keyboard.add(*stuff)
         with open('my_dick.json') as trash:
             goal = json.load(trash)
-        bot.edit_message_text(chat_id = call.from_user.id, message_id = goal[str(user_id)]["id_of_message"], text = 'Выбери необходимое количество новостей:', reply_markup = keyboard)
+        bot.edit_message_text(chat_id = call.from_user.id, message_id = goal[str(user_id)]["id_of_message"], text = 'Select the required number of news items:', reply_markup = keyboard)
     if call.data.split('_')[0] == 'amount':
         users = json_communicate('r', 'my_dick.json')
         users[str(call.from_user.id)]['amount'] = call.data.split('_')[1]
         json_communicate('w', 'my_dick.json', users)
-        bot.send_message(call.message.chat.id, text ='Напиши слово "новости" :)')
+        with open('my_dick.json') as trash:
+            goal = json.load(trash)
+        bot.edit_message_text(chat_id = call.from_user.id, message_id = goal[str(user_id)]["id_of_message"], text ='Write the word "news" :)')
 
 def json_communicate(method, file, data=None):
     if method == 'r':
@@ -75,29 +87,41 @@ def json_communicate(method, file, data=None):
         with open(file, 'w') as info:
             json.dump(data, info)
         return True
-    
+
 @bot.message_handler(content_types=['text'])
 def get_message(message):
-    if message.text.lower().split()[0] == 'новости':
+    if message.text.lower().split()[0] == 'news':
         get_news(message)
     else:
-        bot.send_message(message.from_user.id, 'Вы не написали первым слово "новости" :(')
-    
+        search_text = '%20'.join(''.join([i for i in message.text if (i.isalpha() or i == ' ')]).split())
+        site = requests.get('https://yandex.ru/images/search?text=' + search_text)
+        if str(site) == '<Response [200]>':
+            print(site.text)
+            data = bs(site.text, features='lxml')
+            result = data.find_all('img', class_='serp-item__thumb justifier__thumb')
+            if not result:
+                bot.send_message(message.from_user.id, "I couldn't find any pictures of this")
+            else:
+                choosen_picture = 'https:' + rd.choice(result).get('src')
+                bot.send_photo(message.from_user.id, choosen_picture, 'Get it :)')
+        else:
+            bot.send_message(message.from_user.id, 'Everything is broken :(')
+            
 
 def get_news(message):
     user_id = message.from_user.id
     with open('my_dick.json') as trash:
         goal = json.load(trash)
-    land = goal[str(user_id)]["country"]
+    land = goal[str(user_id)]["country"].lower()
     topic = goal[str(user_id)]["category"]
-    quantity = int(goal[str(user_id)]["amount"])
     required_date = datetime.now().date()
     answer = requests.get(f'https://newsapi.org/v2/everything?q={topic}%20of%20{land}&from={required_date}&to={required_date}&sortBy=popularity&apiKey=52a2b572ebf549149b492212fa1d789d')
     fuck = str(answer.text)
     replace_answer = fuck.replace('null','None')
     dct = eval(replace_answer)
+    quantity = int(goal[str(user_id)]["amount"]) if len(dct['articles']) > int(goal[str(user_id)]["amount"]) else len(dct['articles'])
     parsing(message, dct, quantity)
-    
+
 def parsing(message, dct, quantity):
     for s in range(quantity):
         resource = dct['articles'][s]['source']['name']
@@ -105,13 +129,9 @@ def parsing(message, dct, quantity):
         content = dct['articles'][s]['description']
         link = dct['articles'][s]['url']
         result = 'From '+resource+'\n\n'+running_line.upper()+'\n\n'+content+'\n\n'+link
-        print(dct)
         bot.send_message(message.from_user.id, result)
-    
+
 bot.polling(none_stop=True, interval=0)
-
-
-# In[ ]:
 
 
 
